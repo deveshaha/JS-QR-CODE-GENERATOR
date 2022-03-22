f4d0564368a293ee70cd086ef200c990baf83c2c
Devesh <devesh_hanumante@outlook.com>
2022-03-22 08:52:53 +0100

    Update QR Code Size

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ generateBtn.addEventListener("click", () => {
     if(!qrValue) return;
     generateBtn.innerText = "Generating QR Code...";
 
-    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}`;
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
 
     qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
